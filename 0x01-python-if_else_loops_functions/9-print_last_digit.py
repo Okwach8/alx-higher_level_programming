a81a612e688481e485b1cc6560f578e80eaef2a0
Okwach8 <99367163+Okwach8@users.noreply.github.com>
2022-06-01 05:42:55 +0300

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = abs(number) % 10
-    print(number, end='')
-    return number
-    
+    if number < 0:
+        print("{:d}".format(-(number % -10)), end='')
+        return(-(number % -10))
+    else:
+        print("{:d}".format(number % 10), end='')
+        return(number % 10)
