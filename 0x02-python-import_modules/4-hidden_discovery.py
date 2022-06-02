8376139d685d31d3d59d8009021204bfc4d65c66
Okwach8 <99367163+Okwach8@users.noreply.github.com>
2022-06-03 02:58:28 +0300

    import & modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print(i)
+        if not i.startswith("__"):
+            print("{}".format(i))
