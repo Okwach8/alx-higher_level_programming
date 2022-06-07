e3974f44089d0cc6ab213fffe14074953a3fb110
Okwach8 <99367163+Okwach8@users.noreply.github.com>
2022-06-07 05:15:20 +0300

    Data Structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            new += my_string[i]
-    return new
+    return ''.join(c for c in my_string if c not in 'cC')
