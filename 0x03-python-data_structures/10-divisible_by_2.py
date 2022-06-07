402757dd778f98dd965e5bfd6580edb6e13a3d5e
Okwach8 <99367163+Okwach8@users.noreply.github.com>
2022-06-07 05:19:54 +0300

    Data Structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new = []
-    for i in my_list:
-        new = new + ([True] if not i % 2 else [False])
-    return new
+    return [not x & 1 for x in my_list]
