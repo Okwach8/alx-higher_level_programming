a9b2babf3fedae02568e44bc6ae42d68a019be50
Okwach8 <westher188@gmail.com>
2022-07-05 15:00:03 +0300

    Input/Output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-"""modue for p_tri"""
+"""Module 14-pascal_triangle.
+Returns a list of lists of integers
+representing the Pascal’s triangle of n.
+"""
 
 
 def pascal_triangle(n):
-    """to make this tri"""
-    tri = []
-    line = [1]
-    y = [0]
-    for x in range(max(n, 0)):
-        tri.append(line)
-        line = [l + r for l, r in zip(line + y, y + line)]
-    return tri
+    """Returns the pascal triangle of n.
+
+    Args:
+        - n: size of the triangle (rows)
+
+    Returns: a list of list of integers
+    """
+
+    if n <= 0:
+        return []
+
+    l = [[0 for x in range(i + 1)] for i in range(n)]
+    l[0] = [1]
+
+    for i in range(1, n):
+        l[i][0] = 1
+        for j in range(1, i + 1):
+            if j < len(l[i - 1]):
+                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
+            else:
+                l[i][j] = l[i - 1][0]
+    return l
