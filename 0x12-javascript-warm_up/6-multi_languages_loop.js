6cec223df51a29cae6f35be96db7a8c43c5c0ce2
Okwach8 <westher188@gmail.com>
2022-09-06 13:12:35 +0300

    Js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myLines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < myLines.length; i++) {
-  console.log(myLines[i]);
+const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < line.length; i++) {
+  console.log(line[i]);
 }
