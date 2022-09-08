837919a59fd902dfc577f893d87bfa6474dec048
Okwach8 <99367163+Okwach8@users.noreply.github.com>
2022-09-08 04:50:08 +0300

    Js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const answer = [];
-  
-    for (let i = 0; i < list.length; i++) {
-      answer.unshift(list[i]);
-    }
-  
-    return answer;
-  };
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
+    i++;
+    len--;
+  }
+  return list;
+};
