50ef213c7ca5d944218820f282fba888c1007edb
Okwach8 <99367163+Okwach8@users.noreply.github.com>
2022-09-23 18:51:22 +0300

    NT

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" module that fetches https://intranet.hbtn.io/status"""
-import urllib.request as request
+""" Fetches 'https://intranet.hbtn.io/status' """
 
-
-if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as result:
-        body = result.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
-- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        content = r.read()
+    print('Body response:\n\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
+    print('\t- utf8 content: {}'.format(content.decode('utf-8')))
